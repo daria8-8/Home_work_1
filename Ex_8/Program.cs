037e34cfbdc78e69766e91305de5a875e131061a
Daria <daryaneronova@yandex.ru>
2022-09-18 19:42:41 +0300

    Ex_8

---
+++ Ex_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите количество элементов массива: ");
+int [] massiv = new int[999999];
+int N = int.Parse(Console.ReadLine());
+for (int i = 0; i < N; i++)
+massiv[i] = i+1;
+Console.WriteLine("Массив из чётных чисел: ");
+for (int i = 0; i < N; i++)
+if((massiv[i] % 2) == 0)
+Console.Write( "{0} ", massiv[i] );
+
+
+
