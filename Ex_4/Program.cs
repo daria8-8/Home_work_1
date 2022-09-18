a29787dc15409129f24f82089fab29c750751b25
Daria <daryaneronova@yandex.ru>
2022-09-18 19:41:14 +0300

    Ex_4

---
+++ Ex_4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите три числа : ");
+Console.WriteLine("Число a : ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Число b : ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Число c : ");
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
