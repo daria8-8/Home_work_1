7984c1002fe9b6c5111eb16c7d4dc5fe10de86b1
Daria <daryaneronova@yandex.ru>
2022-09-18 19:41:42 +0300

    Ex_6

---
+++ Ex_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число : ");
+int a = int.Parse(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.Write("Число чётное");
+}
+else
+{
+    Console.Write("Число нечётное");
+}
