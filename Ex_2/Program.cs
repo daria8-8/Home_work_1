663e473e76007d993117e9aa1d15b9f13a8e5235
Daria <daryaneronova@yandex.ru>
2022-09-18 19:40:36 +0300

    Ex_2

---
+++ Ex_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите два числа : ");
+Console.WriteLine("Число a : ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Число b : ");
+int b = int.Parse(Console.ReadLine());
+Console.Write(" a = " + a);
+Console.Write(" b = " + b);
+if(a > b)
+{
+     Console.Write(" max = " + a);
+     Console.Write(" min = " + b);
+}
+else if(a < b)
+{
+     Console.Write(" max = " + b);
+     Console.Write(" min = " + a);
+}
+else
+{
+    Console.WriteLine(" Числа a и b равны ");
+}
+
